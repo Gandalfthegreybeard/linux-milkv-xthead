8806add65151d41cf0ef3a2374b5eca9b7d1af80
TJ Snider <timsn@thtree.com>
2024-04-22 19:46:25 -0700

    use some of RISCV_HWPROBE values for XTHEAD

--- arch/riscv/include/asm/hwprobe.h
+++ arch/riscv/include/asm/hwprobe.h
@@ -8,7 +8,7 @@
 
 #include <uapi/asm/hwprobe.h>
 
-#define RISCV_HWPROBE_MAX_KEY 6
+#define RISCV_HWPROBE_MAX_KEY 7
 
 static inline bool riscv_hwprobe_key_is_valid(__s64 key)
 {
@@ -21,6 +21,7 @@ static inline bool hwprobe_key_is_bitmask(__s64 key)
 	case RISCV_HWPROBE_KEY_BASE_BEHAVIOR:
 	case RISCV_HWPROBE_KEY_IMA_EXT_0:
 	case RISCV_HWPROBE_KEY_CPUPERF_0:
+	case RISCV_HWPROBE_KEY_XTHEAD:
 		return true;
 	}
 
